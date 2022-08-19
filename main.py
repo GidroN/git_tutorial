8157f6f1764565b2c7f4ea17acdf1aea58610b4b
gidron <i@gidroonn.ru>
2022-08-19 13:12:15 +0400

    Made a Calculator class for better use.

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 #! some module
 
-def add(x: int, y: int) -> int:
-    return x + y
+class Calculator:
+    
+    def add(x: int, y: int) -> int:
+        return x + y
 
-def sub(x: int, y: int) -> int:
-    return x - y
+    def sub(x: int, y: int) -> int:
+        return x - y
 
-def mult(x: int, y: int) -> int:
-    return x * y
+    def mult(x: int, y: int) -> int:
+        return x * y
 
-def div(x: int, y: int) -> int:
-    if y == 0:
-        raise ZeroDivisionError()
-    return x // y
+    def div(x: int, y: int) -> int:
+        if y == 0:
+            raise ZeroDivisionError()
+        return x // y
 
 def main():
     x, y = map(int, input('> ').split())
-    sub(x, y)
-    add(x, y)
-    mult(x, y)
-    div(x, y)
+    calc = Calculator()
+    calc.sub(x, y)
+    calc.add(x, y)
+    calc.mult(x, y)
+    calc.div(x, y)
 
 if __name__ == '__main__':
     main()
